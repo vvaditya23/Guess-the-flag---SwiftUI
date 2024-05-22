b85fb6a2647c4bc1c151dbf72ea3605bec629bce
Aditya Vyavahare <vvaditya23@icloud.com>
2024-05-22 15:52:45 +0530

    🔼  Upgraded design

--- Guess the flag/ContentView.swift
+++ Guess the flag/ContentView.swift
@@ -18,24 +18,45 @@ struct ContentView: View {
         ZStack {
             LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
-            VStack(spacing: 15) {
-                Text("Tap the flag of")
-                    .foregroundStyle(.white)
-                    .font(.subheadline.weight(.heavy))
-                Text(countries[correctAnswer])
-                    .foregroundStyle(.white)
-                    .font(.largeTitle.weight(.heavy))
+            VStack {
+                Spacer()
                 
-                ForEach(0..<3) { number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        Image(countries[number])
-                            .clipShape(.capsule)
-                            .shadow(radius: 5)
+                Text("Guess the flag")
+                    .font(.largeTitle.weight(.heavy))
+                    .foregroundStyle(.white)
+                VStack(spacing: 15) {
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                        .font(.subheadline.weight(.heavy))
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                        .font(.largeTitle.weight(.heavy))
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 10)
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.ultraThinMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: ???")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                
+                Spacer()
             }
+            .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
