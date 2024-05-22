fb82ae07ad7bf75c246b805067b2a435a4943238
Aditya Vyavahare <vvaditya23@icloud.com>
2024-05-22 13:58:33 +0530

    Initial Commit

---
+++ Guess the flag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guess the flag
+//
+//  Created by Aditya Vyavahare on 22/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
